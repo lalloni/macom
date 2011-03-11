58400fdcfddd8cb6fe46152ec97bf3acf1fd875c
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2011-03-11 13:39:28 -0300

    campos de módulo que se muestran en la grilla

--- src/macom/diagrama/admin.py
+++ src/macom/diagrama/admin.py
@@ -6,6 +6,7 @@ from macom.diagrama import models
 class ModuleAdmin(admin.ModelAdmin):
     fields = ['system', 'name', 'goal', 'external', 'criticity', 'exposed', 'consumed' ]
     filter_horizontal = ['consumed', 'exposed']
+    list_display = ['system', 'name', 'goal', 'external']
 
 admin.site.register(models.Interface)
 admin.site.register(models.Module, ModuleAdmin)
