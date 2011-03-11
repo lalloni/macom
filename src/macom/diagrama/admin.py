4b4001012fd3daf57833a436f1c1f84a3e2dc74d
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2011-03-11 13:36:24 -0300

    orden de los campos en el modulo

--- src/macom/diagrama/admin.py
+++ src/macom/diagrama/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from macom.diagrama import models
 
 class ModuleAdmin(admin.ModelAdmin):
+    fields = ['system', 'name', 'goal', 'external', 'criticity', 'exposed', 'consumed' ]
     filter_horizontal = ['consumed', 'exposed']
 
 admin.site.register(models.Interface)
