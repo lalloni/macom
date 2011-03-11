db95b93d4baedd591eca8db93d8b1a96aa98f8bb
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2011-03-11 15:12:50 -0300

    cambios en el modelo y la view

--- src/macom/diagrama/admin.py
+++ src/macom/diagrama/admin.py
@@ -2,12 +2,32 @@
 
 from django.contrib import admin
 from macom.diagrama import models
+from django.utils.translation import ugettext_lazy as _
 
+class InlineInterface(admin.TabularInline):
+    model = models.Interface
+    fields = ['name', 'goal', 'technology']
+    verbose_name = _('Interface expuesta')
+    verbose_name_plural = _('Interfaces expuestas')
+    extra = 0
+            
 class ModuleAdmin(admin.ModelAdmin):
-    fields = ['system', 'name', 'goal', 'external', 'criticity', 'exposed', 'consumed' ]
-    filter_horizontal = ['consumed', 'exposed']
+    fields = ['system', 'name', 'goal', 'external', 'criticity', 'consumed' ]
+    filter_horizontal = ['consumed']
     list_display = ['system', 'name', 'goal', 'external']
+    list_display_links = ['name']
+    search_fields = ['name', 'goal']
+    inlines = [InlineInterface]
 
-admin.site.register(models.Interface)
+class SystemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    
+class InterfaceAdmin(admin.ModelAdmin):
+    list_display = ['exposer', 'name', 'goal']
+    list_display_links = ['name']
+    ordering = ['exposer']
+    search_fields = ['name']
+    
+admin.site.register(models.Interface, InterfaceAdmin)
 admin.site.register(models.Module, ModuleAdmin)
-admin.site.register(models.System)
+admin.site.register(models.System, SystemAdmin)
--- src/macom/diagrama/models.py
+++ src/macom/diagrama/models.py
@@ -6,6 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 class System(models.Model):
     name = models.CharField(_('name'), help_text=_('name-help'), max_length=100)
     description = models.CharField(_('description'), help_text=_('description-help'), max_length=200)
+    external = models.BooleanField(_('Externo'))
 
     class Meta:
         verbose_name = _('system')
@@ -25,8 +26,7 @@ class Module(models.Model):
     goal = models.CharField(_('goal'), help_text=_('goal-help'), max_length=200, blank=True)
     system = models.ForeignKey(System, verbose_name=_('system'), null=True, blank=True)
     external = models.BooleanField(_('external'))
-    consumed = models.ManyToManyField('Interface', verbose_name = 'Interfaces Consumidas', blank=True, related_name='consumed')
-    exposed = models.ManyToManyField('Interface', verbose_name = 'Interfaces Expuestas', blank=True, related_name='exposed')
+    consumed = models.ManyToManyField('Interface', verbose_name = _('Interfaces Consumidas'), blank=True, related_name='consumers')
     criticity = models.CharField(_('criticity'), help_text=_('criticity-help'), max_length=2, choices=CRITICITY)
 
     class Meta:
@@ -40,6 +40,7 @@ class Interface(models.Model):
     name = models.CharField(_('name'), help_text=_('name-help'), max_length=100)
     goal = models.CharField(_('goal'), help_text=_('goal-help'), max_length=200)
     technology = models.CharField(_('technology'), help_text=_('technology-help'), max_length=200)
+    exposer = models.ForeignKey('Module', verbose_name = _('exposer'), related_name='exposed', null=True)
 
     class Meta:
         verbose_name = _('interface')
