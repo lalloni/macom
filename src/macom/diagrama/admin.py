8d2c73bfc51b6b8f656194d740572f4c89f5d30f
pnb <pnb@pnb-work.(none)>
2011-09-20 15:58:48 -0300

    Agregada migracion con south
    Agregados nuevos campos:
     * Documentación
     * Referentes
     * Data Inbound
     * Data Outbound
    
    Cambio de tipo de dato en goal, description y technology a TextField

--- src/macom/diagrama/admin.py
+++ src/macom/diagrama/admin.py
@@ -6,28 +6,30 @@ from django.utils.translation import ugettext_lazy as _
 
 class InlineInterface(admin.TabularInline):
     model = models.Interface
-    fields = ['name', 'goal', 'technology']
+    fields = ['exposer', 'name', 'goal', 'technology', 'direction_inbound', 'direction_outbound']
+    list_display = ['exposer', 'name', 'goal', 'technology']
     verbose_name = _('Interface expuesta')
     verbose_name_plural = _('Interfaces expuestas')
     extra = 0
             
 class ModuleAdmin(admin.ModelAdmin):
-    fields = ['system', 'name', 'goal', 'external', 'criticity', 'consumed' ]
+    fields = ['system', 'name', 'goal', 'referents', 'documentation', 'external', 'criticity', 'consumed' ]
     filter_horizontal = ['consumed']
     list_display = ['system', 'name', 'goal', 'external']
     list_display_links = ['name']
-    search_fields = ['name', 'goal']
+    search_fields = ['system__name','name', 'goal']
     inlines = [InlineInterface]
 
 class SystemAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description', 'external']
+    list_display = ['name', 'description', 'referents', 'documentation', 'external']
+    search_fields = ['name', 'description']
     
 class InterfaceAdmin(admin.ModelAdmin):
-    list_display = ['exposer', 'name', 'goal']
+    list_display = ['exposer', 'name', 'goal', 'referents', 'documentation']
     list_display_links = ['name']
     ordering = ['exposer']
-    search_fields = ['name']
-    
+    search_fields = ['exposer__name', 'name']
+
 admin.site.register(models.Interface, InterfaceAdmin)
 admin.site.register(models.Module, ModuleAdmin)
 admin.site.register(models.System, SystemAdmin)
