6efb0a4a183e070e97aef82026b4cf61c0136606
Pablo Lalloni <plalloni@gmail.com>
2011-10-17 14:40:36 -0300

    Formalize app l&f

--- src/macom/app/templates/app.js
+++ src/macom/app/templates/app.js
@@ -2,40 +2,49 @@ isc.DataSource.create({
 	ID : "ds_model",
 	dataURL : "{% url api_model %}",
 	dataFormat : "json",
-	fields : [
-        { name : "name" },
-    ]
+	fields : [ {
+		name : "name"
+	}, ]
 });
 
 isc.VLayout.create({
 	width : "100%",
 	height : "100%",
 	members : [ isc.HStack.create({
-		ID : "HeaderBar",
-		height : "30px",
+		ID : "HeaderSection",
+		height : 48,
+		hPolicy : "fill",
 		members : [ isc.Label.create({
-			contents : "macom"
+			contents : "macom",
+			baseStyle : "header"
+		}), isc.LayoutSpacer.create({
+			width : "*"
+		}), isc.Img.create({
+			src : "grass.png",
+			imageType : "normal"
 		}) ]
 	}), isc.HLayout.create({
+		ID: "ContentSection",
 		height : "*",
+		showEdges : "true",
 		members : [ isc.TreeGrid.create({
-            width: '300px',
-            dataSource : "ds_model",
+			ID:"NavigationTree",
+			width : 300,
+			dataSource : "ds_model",
 			autoFetchData : true,
-            loadDataOnDemand: false,
-            defaultIsFolder: false,
+			loadDataOnDemand : false,
+			defaultIsFolder : false,
 			showResizeBar : true,
-            generateClickOnEnter: true,
-            fields: [
-                { name: 'name' ,
-                    recordDoubleClick: openTab }
-            ],
-            }), isc.TabSet.create({
-                ID: 'contentTabs'
-            }) 
-        ]
+			generateClickOnEnter : true,
+			fields : [ {
+				name : 'name',
+				recordDoubleClick : openTab
+			} ],
+		}), isc.TabSet.create({
+			ID : 'ContentTabSet'
+		}) ]
 	}), isc.HStack.create({
-		ID : "BottomToolBar",
+		ID : "FooterSection",
 		height : "1",
 		members : [ isc.IButton.create({
 			title : "Console",
@@ -44,103 +53,183 @@ isc.VLayout.create({
 	}) ]
 });
 
-function openTab (viewer, record, recordNum, field, fieldNum, value, rawValue) {
-    // buscar tab q tenga el mismo record
-    var tab;
-    for ( i=0; i < contentTabs.tabs.length; i++ ) {
-        var t = contentTabs.getTab(i);
-        if ( t.record == record ) {
-            tab = t;
-            break;
-        }
-    }
-    
-    // si no se encuentra generar uno nuevo con titulo = record.name
-    if ( !tab ){
-        contentTabs.addTab( {
-            title: getTitle(record) + rawValue,
-            record : record,
-            canClose: true,
-            pane: nodeContent(record)
-        } );
-        tab = contentTabs.getTab(contentTabs.tabs.length-1);
-    }
+function openTab(viewer, record, recordNum, field, fieldNum, value, rawValue) {
+	// buscar tab q tenga el mismo record
+	var tab;
+	for (i = 0; i < contentTabs.tabs.length; i++) {
+		var t = contentTabs.getTab(i);
+		if (t.record == record) {
+			tab = t;
+			break;
+		}
+	}
 
-    contentTabs.selectTab( tab );
+	// si no se encuentra generar uno nuevo con titulo = record.name
+	if (!tab) {
+		contentTabs.addTab({
+			title : getTitle(record) + rawValue,
+			record : record,
+			canClose : true,
+			pane : nodeContent(record)
+		});
+		tab = contentTabs.getTab(contentTabs.tabs.length - 1);
+	}
+
+	contentTabs.selectTab(tab);
 }
 
-function getTitle( record ){
-    if ( record._parent_isc_ResultTree_0.name ) return getTitle( record._parent_isc_ResultTree_0 ) + record._parent_isc_ResultTree_0.name + ":";
-    return "";
+function getTitle(record) {
+	if (record._parent_isc_ResultTree_0.name)
+		return getTitle(record._parent_isc_ResultTree_0)
+				+ record._parent_isc_ResultTree_0.name + ":";
+	return "";
 }
 
-function nodeContent( record ) {
-    switch ( record.type ) {
-        case 'system':
-            return isc.DynamicForm.create({
-                    autoFetchData: true,
-                    canEdit: false,
-                    dataSource: isc.DataSource.create({
-                        dataURL: record.readurl,
-                        dataFormat : "json",
-                        dataProtocol: "getParams"
-                    }),
-                    fields: [
-                        { name:"name", type:"text", title:"Nombre", width: "*"},
-                        { name:"description", type:"textArea", title:"Descripción", width: "*"},
-                        { name:"referents", type:"textArea", title:"Referentes", width: "*"},
-                        { name:"documentation", type:"textArea", title:"Documentación", width: "*"},
-                        { name:"external", type:"checkbox", title:"Externo", width: "*"}
-                    ]
-            });
+function nodeContent(record) {
+	switch (record.type) {
+	case 'system':
+		return isc.DynamicForm.create({
+			autoFetchData : true,
+			canEdit : false,
+			dataSource : isc.DataSource.create({
+				dataURL : record.readurl,
+				dataFormat : "json",
+				dataProtocol : "getParams"
+			}),
+			fields : [ {
+				name : "name",
+				type : "text",
+				title : "Nombre",
+				width : "*"
+			}, {
+				name : "description",
+				type : "textArea",
+				title : "Descripción",
+				width : "*"
+			}, {
+				name : "referents",
+				type : "textArea",
+				title : "Referentes",
+				width : "*"
+			}, {
+				name : "documentation",
+				type : "textArea",
+				title : "Documentación",
+				width : "*"
+			}, {
+				name : "external",
+				type : "checkbox",
+				title : "Externo",
+				width : "*"
+			} ]
+		});
 
-            break;
-        case 'module':
-            return isc.DynamicForm.create({
-                    autoFetchData: true,
-                    canEdit: false,
-                    dataSource: isc.DataSource.create({
-                        dataURL: record.readurl,
-                        dataFormat : "json",
-                        dataProtocol: "getParams",
-                        fields: [
-                            { name: "system", valueXPath: "system/name" }
-                        ]
-                    }),
-                    fields: [
-                        { name:"system", type:"textArea", title:"Sistema", width: "*"},    
-                        { name:"name", type:"text", title:"Nombre", width: "*"},
-                        { name:"goal", type:"textArea", title:"Objetivo", width: "*"},
-                        { name:"referents", type:"textArea", title:"Referentes", width: "*"},
-                        { name:"documentation", type:"textArea", title:"Documentación", width: "*"},
-                        { name:"external", type:"checkbox", title:"Externo", width: "*"},
-                        { name:"criticity", type:"text",title:"Criticidad"}
-                    ]
-            });
-            break;
-        case 'interface':
-            return isc.DynamicForm.create({
-                    autoFetchData: true,
-                    canEdit: false,
-                    dataSource: isc.DataSource.create({
-                        dataURL: record.readurl,
-                        dataFormat : "json",
-                        dataProtocol: "getParams",
-                        fields: [
-                            { name: "module", valueXPath: "module/name" }
-                        ]
-                    }),
-                    fields: [
-                        { name:"module", type:"text", title:"Módulo", width: "*"},    
-                        { name:"name", type:"text", title:"Nombre", width: "*"},
-                        { name:"goal", type:"textArea", title:"Objetivo", width: "*"},
-                        { name:"referents", type:"textArea", title:"Referentes", width: "*"},
-                        { name:"documentation", type:"textArea", title:"Documentación", width: "*"},
-                        { name:"technology", type:"textArea", title:"Tecnología", width: "*"},
-                        { name:"direction_inbound", type:"checkbox", title:"Entrada", width: "*"},
-                        { name:"direction_outbound", type:"checkbox", title:"Salida", width: "*"},
-                    ]
-            });
-            break;
-    }
+		break;
+	case 'module':
+		return isc.DynamicForm.create({
+			autoFetchData : true,
+			canEdit : false,
+			dataSource : isc.DataSource.create({
+				dataURL : record.readurl,
+				dataFormat : "json",
+				dataProtocol : "getParams",
+				fields : [ {
+					name : "system",
+					valueXPath : "system/name"
+				} ]
+			}),
+			fields : [ {
+				name : "system",
+				type : "textArea",
+				title : "Sistema",
+				width : "*"
+			}, {
+				name : "name",
+				type : "text",
+				title : "Nombre",
+				width : "*"
+			}, {
+				name : "goal",
+				type : "textArea",
+				title : "Objetivo",
+				width : "*"
+			}, {
+				name : "referents",
+				type : "textArea",
+				title : "Referentes",
+				width : "*"
+			}, {
+				name : "documentation",
+				type : "textArea",
+				title : "Documentación",
+				width : "*"
+			}, {
+				name : "external",
+				type : "checkbox",
+				title : "Externo",
+				width : "*"
+			}, {
+				name : "criticity",
+				type : "text",
+				title : "Criticidad"
+			} ]
+		});
+		break;
+	case 'interface':
+		return isc.DynamicForm.create({
+			autoFetchData : true,
+			canEdit : false,
+			dataSource : isc.DataSource.create({
+				dataURL : record.readurl,
+				dataFormat : "json",
+				dataProtocol : "getParams",
+				fields : [ {
+					name : "module",
+					valueXPath : "module/name"
+				} ]
+			}),
+			fields : [ {
+				name : "module",
+				type : "text",
+				title : "Módulo",
+				width : "*"
+			}, {
+				name : "name",
+				type : "text",
+				title : "Nombre",
+				width : "*"
+			}, {
+				name : "goal",
+				type : "textArea",
+				title : "Objetivo",
+				width : "*"
+			}, {
+				name : "referents",
+				type : "textArea",
+				title : "Referentes",
+				width : "*"
+			}, {
+				name : "documentation",
+				type : "textArea",
+				title : "Documentación",
+				width : "*"
+			}, {
+				name : "technology",
+				type : "textArea",
+				title : "Tecnología",
+				width : "*"
+			}, {
+				name : "direction_inbound",
+				type : "checkbox",
+				title : "Entrada",
+				width : "*"
+			}, {
+				name : "direction_outbound",
+				type : "checkbox",
+				title : "Salida",
+				width : "*"
+			}, ]
+		});
+		break;
+	}
 }
--- src/macom/app/urls.py
+++ src/macom/app/urls.py
@@ -3,10 +3,12 @@ Created on 06/10/2011
 
 @author: plalloni
 '''
+from os import path
 from django.conf.urls.defaults import patterns, url
 from macom.app.views import app, script_template
 
 urlpatterns = patterns('',
     url(r'^$', app, name="app"),
     url(r'^(?P<script_name>.*\.js)$', script_template, name='script_template'),
+    url(r'^images/(?P<path>.*)$', 'django.views.static.serve', {'document_root': path.join(path.dirname(__file__), 'images')})
 )
