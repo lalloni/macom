d8fcc7cb931eb96e233af9933994ca92db5f7ef6
Pablo Baratto <gpbaratto@afip.gob.ar>
2011-11-01 12:56:03 -0300

    Mejora enel default de los handlers. Se agrego id y absolute_uri generico

--- src/macom/api/handlers.py
+++ src/macom/api/handlers.py
@@ -27,7 +27,6 @@ class Defaults(BaseHandler):
 class SystemHandler(Defaults):
     model = System
     fields = ('kind', 'name', 'full_name', 'external', 'description', 'referents', 'documentation', ('modules', ()), 'dependents', 'dependencies', 'diagram_uri')
-
     @classmethod
     def dependents(cls, system):
         return Dependency.objects.filter(interface__module__system=system).exclude(module__system=system)
@@ -44,7 +43,6 @@ class ModuleHandler(Defaults):
 
 class InterfaceHandler(Defaults):
     model = Interface
-
     fields = ('kind', 'name', 'full_name', 'goal', 'referents', 'documentation', 'technology', 'direction', 'diagram_uri')
 
 class DependencyHandler(Defaults):
